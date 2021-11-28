6bf76256524af4edc4b8ee511895d6122aedd31a
WoodenDoor <vincent031525@gmail.com>
2021-11-28 23:44:24 +0800

    2021/11/29 新增BottomDrawer

--- app/src/main/java/com/practice/trace/feature_trace/presentation/expense_money/ExpenseMoneyScreen.kt
+++ app/src/main/java/com/practice/trace/feature_trace/presentation/expense_money/ExpenseMoneyScreen.kt
@@ -1,10 +1,5 @@
 package com.practice.trace.feature_trace.presentation.expense_money
 
-import android.app.DatePickerDialog
-import android.content.Context
-import android.os.Build
-import android.widget.DatePicker
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,22 +10,23 @@ import androidx.compose.material.icons.filled.ControlPoint
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.practice.trace.feature_trace.domain.model.Money
 import com.practice.trace.feature_trace.presentation.expense_money.components.ExpenseMoneyItem
+import com.practice.trace.ui.theme.DatePickerBackground
 import com.practice.trace.ui.theme.ExpenseBackground
 import com.practice.trace.ui.theme.ExpenseText
 import com.practice.trace.ui.theme.Primary
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
 
+@ExperimentalMaterialApi
 @Composable
 fun ExpenseMoneyScreen(
     navController: NavController,
@@ -40,79 +36,102 @@ fun ExpenseMoneyScreen(
 
     val calendar = viewModel.date.value
     val dateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.TAIWAN)
-
     val total = viewModel.total.value
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(ExpenseBackground)
-    ) {
-        TopAppBar(
-            title = {
-                Text(
-                    text = dateFormat.format(calendar.time)
-                )
-            },
-            navigationIcon = {
-                IconButton(
-                    onClick = {
-                        /*TODO NavigationDrawer*/
+    val bottomDrawerState = rememberBottomDrawerState(BottomDrawerValue.Closed)
+
+    val scope = rememberCoroutineScope()
+
+    BottomDrawer(
+        gesturesEnabled = true,
+        drawerState = bottomDrawerState,
+        drawerContent = {
+            Row(modifier = Modifier.widthIn(min = 400.dp),
+                horizontalArrangement = Arrangement.Center
+            ){
+                Text(text = "1123")
+                Text(text = "dasfd")
+            }
+        },
+        drawerElevation = 20.dp,
+        drawerBackgroundColor = DatePickerBackground,
+        modifier = Modifier.fillMaxSize(),
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(ExpenseBackground)
+            ) {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = dateFormat.format(calendar.time)
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                /*TODO NavigationDrawer*/
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = ""
+                            )
+                        }
+                    },
+                    backgroundColor = Primary,
+                    contentColor = ExpenseText,
+                    actions = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                bottomDrawerState.open()
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.DateRange,
+                                contentDescription = ""
+                            )
+                        }
+                        IconButton(onClick = {
+                            /*TODO Navigate AddExpenseMoney*/
+                        }) {
+                            Icon(
+                                imageVector = Icons.Filled.ControlPoint,
+                                contentDescription = ""
+                            )
+                        }
                     }
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(all = 10.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    Icon(
-                        imageVector = Icons.Filled.Menu,
-                        contentDescription = ""
+                    Text(
+                        text = "TWD",
+                        color = ExpenseText,
+                        fontSize = 50.sp
+                    )
+                    Text(
+                        text = total.toString(),
+                        color = ExpenseText,
+                        fontSize = 50.sp
                     )
                 }
-            },
-            backgroundColor = Primary,
-            contentColor = ExpenseText,
-            actions = {
-                IconButton(onClick = {
-                    /*TODO Change Date*/
-                }) {
-                    Icon(
-                        imageVector = Icons.Filled.DateRange,
-                        contentDescription = ""
-                    )
-                }
-                IconButton(onClick = {
-                    /*TODO Navigate AddExpenseMoney*/
-                }) {
-                    Icon(
-                        imageVector = Icons.Filled.ControlPoint,
-                        contentDescription = ""
-                    )
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    contentPadding = PaddingValues(horizontal = 10.dp),
+                    verticalArrangement = Arrangement.spacedBy(6.dp),
+                ) {
+                    items(state.expenseMoney) { money ->
+                        ExpenseMoneyItem(money = money)
+                    }
                 }
             }
-        )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(all = 10.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = "TWD",
-                color = ExpenseText,
-                fontSize = 50.sp
-            )
-            Text(
-                text = total.toString(),
-                color = ExpenseText,
-                fontSize = 50.sp
-            )
         }
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth(),
-            contentPadding = PaddingValues(horizontal = 10.dp),
-            verticalArrangement = Arrangement.spacedBy(6.dp),
-        ) {
-            items(state.expenseMoney) { money ->
-                ExpenseMoneyItem(money = money)
-            }
-        }
-    }
+    )
+
 }
